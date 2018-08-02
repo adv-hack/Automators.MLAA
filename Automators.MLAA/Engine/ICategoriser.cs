835cbd86e776b148ef503399168bde7d6faef163
Vedavyas Rao <vedavyas.rao@oneadvanced.com>
2018-08-02 14:47:33 +0530

    Create Engine Interface for data access

---
+++ Automators.MLAA/Engine/ICategoriser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Entity;
+
+namespace Engine
+{
+    public interface ICategoriser
+    {
+        List<TestDataResult> Categorise(List<string> testData);
+    }
+}
--- Automators.MLAA/Entity/FunctionData.cs
+++ Automators.MLAA/Entity/FunctionData.cs
@@ -2,15 +2,16 @@
 
 namespace Entity
 {
-    public class CategoryData
+    public class FunctionData
     {
-        public CategoryData()
+        public FunctionData()
         {
             WordList = new List<string>();
             Score = 0.0;
         }
 
-        public string Category { get; set; }
+        public string Function { get; set; }
+        public List<string> Params { get; set; }
         public List<string> TrainingData { get; set; }
         public List<string> WordList { get; set; }
         public double Probability { get; set; }
