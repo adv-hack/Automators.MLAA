893cbc8af53bbb40eac5293e28b0b216a7958a87
nkumarr <naveen.kumar@oneadvanced.com>
2018-08-02 10:12:30 +0530

    Initial checkins for Dataaccess project
    Initial checkins for Dataaccess project

--- Automators.MLAA/Automator.UI/Form1.cs
+++ Automators.MLAA/Automator.UI/Form1.cs
@@ -12,9 +12,19 @@ namespace Automator.UI
 {
     public partial class Form1 : Form
     {
+
+        private string projectPath = @"E:\HackOverflow2018\Hackathon\Progresso";
+        private string _functionListFilePath;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _functionListFilePath = projectPath + @"\ApplicationFiles\functionList.xml";
+        }
+
+        
     }
 }
