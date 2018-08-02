e076f317de1fe4ac57836194a5763e358f58ef40
Vedavyas Rao <vedavyas.rao@oneadvanced.com>
2018-08-02 10:23:47 +0530

    Define Project Layers

---
+++ Automators.MLAA/Entity/CategoryData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Entity
+{
+    public class CategoryData
+    {
+        public CategoryData()
+        {
+            WordList = new List<string>();
+            Score = 0.0;
+        }
+
+        public string Category { get; set; }
+        public List<string> TrainingData { get; set; }
+        public List<string> WordList { get; set; }
+        public double Probability { get; set; }
+        public double Score { get; set; }
+    }
+}
