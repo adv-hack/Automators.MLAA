36cf8ed7bc8d13b7d5382016c18d631a62c37b1c
nkumarr <naveen.kumar@oneadvanced.com>
2018-08-02 09:59:03 +0530

    Initial checkins for dataaccess project
    Initial checkins for dataaccess project

--- Automators.MLAA/Automator.UI/Form1.Designer.cs
+++ Automators.MLAA/Automator.UI/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(627, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -53,7 +53,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(627, 399);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
